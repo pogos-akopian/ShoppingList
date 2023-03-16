38306cbff4843fe2b7b261946153da129b0e33e1
pogos-akopian <sofinmern@gmail.com>
2023-03-17 00:00:14 +0700

    -testBlankFragment +bug fixes +fragment to MainActivity.kt

--- app/src/main/java/com/pogos/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/pogos/shoppinglist/presentation/MainActivity.kt
@@ -43,8 +43,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun launchFragment(fragment: Fragment) {
+        supportFragmentManager.popBackStack()
         supportFragmentManager.beginTransaction()
-            .add(R.id.shop_item_container, fragment)
+            .replace(R.id.shop_item_container, fragment)
+            .addToBackStack(null)
             .commit()
     }
 
